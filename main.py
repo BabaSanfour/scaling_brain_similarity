829e62a25a197927e287798f5fce1c23af191459
Hamza Abdelhedi <hamza.abdelhedii@gmail.com>
2023-03-06 14:38:11 -0500

    add arg parser

--- main.py
+++ main.py
@@ -1,163 +1,193 @@
-"""
-Perform Grid Search on Training Hyper Parameters for each architecture
-"""
-
+import json
 import time
-import torch
+import tqdm
 import wandb
+import warnings
+import torch
+import torch.nn as nn
+from resnet import resnet
+from data_loader import dataloader
+from config import get_config_parser
+from utils import seed_experiment, get_model_size
 
-from utils.load_data import dataloader
-from utils.train_test_functions import train_network, test_network, save_network_weights
+def train_network(model, criterion, optimizer, epochs, dataset_loader, dataset_sizes, device, wandb, valid=True):
+    best_acc = -1.0 
+    list_trainLoss, list_trainAcc, list_valLoss, list_valAcc  = [], [], [], []
+    since = time.time()
+    for epoch in range(epochs):
+        tqdm.write(f"====== Epoch {epoch} ======>")
+        n_correct = 0 #correct predictions train
+        running_loss = 0.0 # train loss
+        loss = 0.0
+        with torch.set_grad_enabled(True):
+            model.train()
+            for inputs_, labels_ in dataset_loader["train"]:
+                inputs = inputs_.to(device)
+                labels = labels_.to(device)
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                n_correct += (torch.max(outputs, 1)[1].view(labels.data.size()) == labels.data).sum().item()
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item() * inputs.size(0)
+            train_acc = 100. * n_correct/dataset_sizes["train"]
+            train_loss = running_loss / dataset_sizes["train"]
+            tqdm.write(f"== [TRAIN] Epoch: {epoch}, Accuracy: {train_acc:.3f} ==>, Loss: {train_loss:.3f} ==>,")
+            list_trainLoss.append(train_loss)
+            list_trainAcc.append(train_acc)
 
-dic = {1000: [30, 25, 12],
-       500: [27, 13],
-       300: [28, 14],
-       150: [29, 15]
-    }
+        if valid:
+            with torch.no_grad():
+                model.eval()
+                n_dev_correct=0
+                running_loss = 0.0
+                loss = 0.0
+                for inputs_, labels_ in dataset_loader["valid"]:
+                    inputs = inputs_.to(device)
+                    labels = labels_.to(device)
+                    outputs = model(inputs)
+                    n_dev_correct += (torch.max(outputs, 1)[1].view(labels.data.size()) == labels.data).sum().item()
+                    loss = criterion(outputs, labels)
+                    running_loss += loss.item() * inputs.size(0)
+                valid_acc = 100. * n_dev_correct/dataset_sizes["valid"]
+                valid_loss = running_loss / dataset_sizes["valid"]
+                list_valLoss.append(valid_loss)
+                list_valAcc.append(valid_acc)
+                tqdm.write(f"== [VALID] Epoch: {epoch}, Accuracy: {valid_acc:.3f} ==>, Loss: {valid_loss:.3f} ==>,")
+                if valid_acc> best_acc:
+                    best_acc= valid_acc
+                    best_model = model
+            wandb.log({"Val Loss": valid_loss, "Val Acc": valid_acc, 
+                    "Train Loss": train_loss, "Train Acc": train_acc})
+        else:
+            wandb.log({"Train Loss": train_loss, "Train Acc": train_acc})
+    print()
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 3600} h {(time_elapsed % 3600) // 60} m {time_elapsed % 60} s')
+    print(f'BEST VALID ACC: {valid_acc:.3f}')
+    return best_model, wandb
+    
+def test_network(model, dataset_loader, dataset_sizes, device):
+    model.eval()
+    n_dev_correct=0
+    tot_correct_topk=0
+    test_loss = 0.0
+    criterion = nn.CrossEntropyLoss()
+    for inputs_, labels_ in dataset_loader['test']:
+        inputs = inputs_.to(device)
+        labels = labels_.to(device)
+        outputs = model(inputs)
+        _, y_pred = outputs.topk(k=5, dim=1)
+        y_pred = y_pred.t()
+        labels_reshaped = labels.view(1, -1).expand_as(y_pred)
+        correct = (y_pred == labels_reshaped)
+        flattened_indicator_which_topk_matched_truth = correct.reshape(-1).float()
+        tot_correct_topk += flattened_indicator_which_topk_matched_truth.float().sum(dim=0, keepdim=True)
+        n_dev_correct += (torch.max(outputs, 1)[1].view(labels.data.size()) == labels.data).sum().item()
+        loss = criterion(outputs, labels)
+        test_loss += loss.item() * inputs.size(0)
+    avg_test_acc = 100. * n_dev_correct/dataset_sizes["test"]
+    avg_test_loss = test_loss / dataset_sizes["test"]
+    avg_test_acc5 = 100. * tot_correct_topk/dataset_sizes["test"]
+    print(f'TEST LOSS: {avg_test_loss:.3f}')
+    print(f'TEST Accuracy (Top1): {avg_test_acc:.3f}')
+    print(f'TEST Accuracy (Top5): {avg_test_acc5:.3f}')
+    return avg_test_acc
 
-
-path_weights = "/home/hamza97/scratch/net_weights/"
+def save_network_weights(model_ft,  file) :
+    """Save the network after training"""
+    state = model_ft.state_dict()
+    torch.save(state, file)
 
 if __name__ == '__main__':
-    # To work with GPU
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    parser = get_config_parser()
+    args = parser.parse_args()
 
-    num_epochs=51
-    learning_rate=[0.1, 0.01, 0.001] # 0.01 is the best learning rate for all tested models
-    momentum=0.9
-    step_size=10
-    gamma=0.1
-    pretrained=True
-    n_input_channels=1
-    num_classes_list=[1000, 500, 300] # 1000 Gave the best performance across all HP and models
-    # we also tested with 4000 and 10000 but no promising results.
-    #model
-    model_name="alexnet"
-    # select batch sizes: we some of the models couldn't work with big batch sizes because of our gpu
-    if model_name in ["alexnet", "resnet18" "resnet34", "deepID_1", "LightCNN", "cornet_z"]:
-        batch_sizes = [8, 16, 32, 64, 128, 256, 512]
-    elif model_name in ["cornet_s", "resnet50", "mobilenet"]:
-        batch_sizes = [8, 16, 32, 64, 128, 256]
-    elif model_name in ["resnet101", "resnet152", "vgg16_bn", "vgg19_bn"]:
-        batch_sizes = [8, 16, 32, 64, 128]
-    elif model_name in ["inception_v3", "FaceNet", "SphereFace"]:
-        batch_sizes = [8, 16, 32, 64]
-    elif model_name in ["resnet101", "resnet152", "vgg16", "vgg19"]:
-        batch_sizes = [8, 16, 32]
+    # Check for the device
+    if (args.device == "cuda") and not torch.cuda.is_available():
+        warnings.warn(
+            "CUDA is not available, make that your environment is "
+            "running on GPU"
+            'Forcing device="cpu".'
+        )
+        args.device = "cpu"
+
+    if args.device == "cpu":
+        warnings.warn(
+            "You are about to run on CPU, and might run out of memory shortly"
+        )
+
+    # Seed the experiment, for repeatability
+    seed_experiment(args.seed)
 
     start = time.time()
+    # Load model
+    print(f'Build model {args.model.upper()}...')
+    if args.model_config is not None:
+        print(f'Loading model config from {args.model_config}')
+        with open(args.model_config) as f:
+            model_config = json.load(f)
+    else:
+        raise ValueError('Please provide a model config json')
+    print(f'########## {args.model.upper()} CONFIG ################')
+    for key, val in model_config.items():
+        print(f'{key}:\t{val}')
+    print('############################################')
+    model = resnet(**model_config)
+    model.to(args.device)
 
     # Loss function
     criterion = torch.nn.CrossEntropyLoss()
     best_acc=-1.0
-    for num_classes in num_classes_list:
-        for num_pictures in dic[num_classes]:
-            for batch_size in batch_sizes:
-                dataset_loader, dataset_sizes = dataloader(batch_size, n_input_channels)
-                for lr in learning_rate:
-                    wandb.init(
-                            # set the wandb project where this run will be logged
-                            project="Modeling the Face Recognition System in the Brain",
-                            
-                            # track hyperparameters and run metadata
-                            config={
-                            "learning_rate": lr,
-                            "architecture": model_name,
-                            "dataset": "celebA-1000",
-                            "epochs": num_epochs,
-                            "batch_size": batch_size,
-                            "num_classes": num_classes,
-                            "momentum": momentum, 
-                            "step_size": step_size,
-                            "gamma": gamma,
-                            "pretrained": pretrained,
-                            }
-                        )
+    wandb.init(
+            # set the wandb project where this run will be logged
+            project="Scaling AI-Brain similarity",
+            
+            # track hyperparameters and run metadata
+            config={
+            "learning_rate": args.lr,
+            "epochs": args.epochs,
+            "batch_size": args.batch_size,
+            "Optimizer": args.optimizer, 
+            "Total param": get_model_size(model, False),
+            "Trainable param": get_model_size(model, True),
+            }
+        )
 
-                    if model_name == "inception_v3":
-                        from models.inception import inception_v3
-                        model = inception_v3(pretrained, num_classes, n_input_channels)
-                    elif model_name == "alexnet":
-                        from models.alexnet import alexnet
-                        model = alexnet(pretrained, num_classes, n_input_channels)
-                    elif model_name ==  "cornet_s":
-                        from models.cornet_s import cornet_s
-                        model = cornet_s(pretrained, num_classes, n_input_channels)
-                    elif model_name ==  "cornet_z":
-                        from models.cornet_z import cornet_z
-                        model = cornet_z(pretrained, num_classes, n_input_channels)
-                    elif model_name == "mobilenet":
-                        from models.mobilenet import mobilenet_v2
-                        model = mobilenet_v2(pretrained, num_classes, n_input_channels)
-                    elif model_name == "resnet18":
-                        from models.resnet import resnet18
-                        model = resnet18(pretrained, num_classes, n_input_channels)
-                    elif model_name == "resnet34":
-                        from models.resnet import resnet34
-                        model = resnet34(pretrained, num_classes, n_input_channels)
-                    elif model_name == "resnet50":
-                        from models.resnet import resnet50
-                        model = resnet50(pretrained, num_classes, n_input_channels)
-                    elif model_name == "resnet101":
-                        from models.resnet import resnet101
-                        model = resnet101(pretrained, num_classes, n_input_channels)
-                    elif model_name == "resnet152":
-                        from models.resnet import resnet152
-                        model = resnet152(pretrained, num_classes, n_input_channels)
-                    elif model_name == "vgg16":
-                        from models.vgg import vgg16
-                        model = vgg16(pretrained, num_classes, n_input_channels)
-                    elif model_name == "vgg16_bn":
-                        from models.vgg import vgg16_bn
-                        model = vgg16_bn(pretrained, num_classes, n_input_channels)
-                    elif model_name == "vgg19":
-                        from models.vgg import vgg19
-                        model = vgg19(pretrained, num_classes, n_input_channels)
-                    elif model_name == "vgg19_bn":
-                        from models.vgg import vgg19_bn
-                        model = vgg19_bn(pretrained, num_classes, n_input_channels)
-                    elif model_name == "FaceNet":
-                        from models.FaceNet import FaceNet
-                        model = FaceNet(pretrained, num_classes, n_input_channels)
-                    elif model_name == "LightCNN":
-                        from models.LightCNN import LightCNN_V4
-                        model = LightCNN_V4(pretrained, num_classes, n_input_channels)
-                    elif model_name == "deepID_1":
-                        from models.DeepID_pytorch import deepID_1
-                        model = deepID_1(pretrained, num_classes, n_input_channels)
-                    elif model == "SphereFace":
-                        from models.SphereFace import SphereFace
-                        model = SphereFace(pretrained, num_classes, n_input_channels)
 
-                    model.to(device)
+    # Optimizer
+    if args.optimizer == "adamw":
+        optimizer = torch.optim.AdamW(
+            model.parameters(), lr=args.lr, weight_decay=args.weight_decay
+        )
+    elif args.optimizer == "adam":
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    elif args.optimizer == "sgd":
+        optimizer = torch.optim.SGD(
+            model.parameters(), lr=args.lr, weight_decay=args.weight_decay
+        )
+    elif args.optimizer == "momentum":
+        optimizer = torch.optim.SGD(
+            model.parameters(),
+            lr=args.lr,
+            momentum=args.momentum,
+            weight_decay=args.weight_decay,
+        )
+    # model size
+    print(
+        f"Initialized {args.model.upper()} model with {get_model_size(model, False)} "
+        f"total parameters, of which {get_model_size(model, True)} are learnable."
+    )
+    #DATA
+    dataset_loader, dataset_sizes = dataloader(args.batch_size, args.number_pictures_per_class)
 
-                    # Optimizer
-                    optimizer_ft = torch.optim.SGD(model.parameters(), lr, momentum)
+    ###Training & Validation###
+    model_ft, wandb = train_network(model, criterion, optimizer, args.epochs, dataset_loader, dataset_sizes, args.device, wandb)
+    ###Testing###
+    acc=test_network(model_ft, dataset_loader, dataset_sizes, args.device)
+    wandb.log ({"Test Acc": acc})
 
-                    # Decay LR by a factor of 0.1 every 20 epochs
-                    exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_ft, step_size, gamma)
-
-                    name="%s_%sLR_%sBatch_%s_%s"%(model_name, lr, batch_size, num_classes, num_pictures)
-                    print("%%%%%%%%%% new model %%%%%%%%%%%%%%%%")
-                    print(name)
-
-                    ###Training & Validation###
-                    model_ft, wandb = train_network(name, model, criterion, optimizer_ft, exp_lr_scheduler, num_epochs, dataset_loader, dataset_sizes, wandb)
-                    ###Testing###
-                    acc=test_network(model_ft, dataset_loader, dataset_sizes)
-                    wandb.log ({"Test Acc": acc})
-
-                    if acc>best_acc:
-                        best_model=model_ft
-                        best_name=name
-                        best_acc=acc
-                    print('=' * 10)
-
-    print()
-    time_training = time.time() - start
-    print('Training ended in %s h %s m %s s' % (time_training // 3600, (time_training % 3600) // 60, time_training % 60))
 
     #  Save weights after training
-    print('best model: ', best_name)
-    print('best model accuracy: ', best_acc)
-    save_network_weights(best_model,best_name)
+    save_network_weights(model,"todo")
