d83a2a2d83f81fbf2eb7ccdd9211bdce2bb6ba5e
Hamza <hamza.abdelhedi@ieee.org>
2023-09-04 17:29:01 -0700

    clean, add docstrings, and combine train and valid scripts

--- data/ImageNet_train_data_to_h5.py
+++ data/ImageNet_train_data_to_h5.py
@@ -1,96 +1,144 @@
-"""
-    For each set file:
-    - Create a HDF5 file with pictures and their IDs.
-    ---------------
-    Output Files:
-    ---------------
-    four HDF5 files for each data training-repartitions: 1, 10, 100, 1000 samples per class
-    Parameters:
-    images       images array, (N, 224, 224, 3) to be stored
-    labels       labels array, (N, ) to be stored
-
-"""
-
 import os
-import cv2
+import sys
+import json
 import h5py
-
 import datetime
-import numpy as np
 from tqdm import tqdm
+
+import numpy as np
+import pandas as pd
+
+import cv2
 import torchvision
 from PIL import Image
 
-def store_many_hdf5(images, labels, folder):
-    """ Stores an array of images to HDF5.
-        Parameters:
-        ---------------
-        images       images array, (N, 224, 224, 3) to be stored
-        labels       labels array, (N, ) to be stored
+import logging
+
+proj_path = sys.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Configure logging
+logging.basicConfig(filename='data_processing.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Load CSV data
+csv_data = pd.read_csv(os.path.join(proj_path, "LOC_val_solution.csv"))
+
+# Load label matching data
+with open(os.path.join(proj_path, "match_labels.json"), 'r') as f:
+    match_labels = json.load(f)
+
+
+def store_many_hdf5(images: np.array, labels: np.array, folder: str) -> None:
+    """Stores an array of images to HDF5.
+
+    Args:
+    images: np.ndarray
+        Images array, shape (N, 224, 224, 3), to be stored.
+    labels: np.ndarray
+        Labels array, shape (N,), to be stored.
+    folder: str
+        Folder name for the HDF5 file.
+
     """
     hdf5_dir = "/home/hamza97/scratch/data/scaling_data/hdf5/"
     if not os.path.exists(hdf5_dir):
         os.makedirs(hdf5_dir)
     # Create a new HDF5 file
-    file = h5py.File("%s%s.h5"%(hdf5_dir,folder), "w")
-    print("{} h5 file created".format(folder))
+    file = h5py.File(os.path.join(hdf5_dir, f"{folder}.h5"), "w")
+    logging.info(f"{folder} h5 file created")
 
-    # Create a dataset in the file
-    dataset = file.create_dataset("images", np.shape(images), data=images) #h5py.h5t.STD_U8BE,
+    # Create datasets in the file
+    dataset = file.create_dataset("images", np.shape(images), data=images)
     metaset = file.create_dataset("meta", np.shape(labels), data=labels)
     file.close()
-    print("{} h5 is ready".format(folder))
+    logging.info(f"{folder} h5 is ready")
 
-def make_array(dir):
+
+def make_array(data_dir: str, folder: str) -> tuple:
+    """
+    Create arrays of images and labels based on the specified analysis type and folder.
+
+    Args:
+        data_dir (str): The path to the data directory.
+        folder (str): The folder type ('train' or 'valid').
+
+    Returns:
+        tuple: A tuple containing image and label arrays.
+    """
     # Concatenate array of images
     img_array = []
     label_array = []
 
-    # resize images to 224 224
+    # Resize images to 224x224
     resize = torchvision.transforms.Resize((224, 224))
-    # extract all the folders names: ids
-    samples_pathes = sorted(
-        [
-            os.path.join(dir, sname)
-            for sname in os.listdir(dir)
-        ]
-    )
-    # run through the ids list / folders list
-    loop_generator = tqdm(samples_pathes)
-    i =0
-    for  id in loop_generator:
-        # extract all the pictures of a single id / in a folder
-        pictures_pathes = sorted(
+
+    if folder == 'train':
+        label_folders = sorted(
             [
-                os.path.join(id, sname)
-                for sname in os.listdir(id)
+                os.path.join(data_dir, label)
+                for label in os.listdir(data_dir)
+                if os.path.isdir(os.path.join(data_dir, label))
             ]
         )
-        # run through the pictures list
-        pictures_loop_generator = tqdm(pictures_pathes)
-        for  picture in pictures_loop_generator:
-            img_sample = cv2.imread(picture) # read picture
+        label = match_labels.get(label_folder, None)
+
+        for label_folder in label_folders:
+            pictures_paths = sorted(
+                [
+                    os.path.join(label_folder, sname)
+                    for sname in os.listdir(label_folder)
+                ]
+            )
+
+            for picture_path in tqdm(pictures_paths):
+                img_sample = cv2.imread(picture_path)  # Read picture
+                if img_sample is None:
+                    continue
+                img_sample = img_sample[:, :, ::-1]  # Transform from BGR to RGB
+                PIL_image = Image.fromarray(img_sample.astype('uint8'), 'RGB')
+                sample = np.asarray(resize(PIL_image), dtype=np.uint8)
+                img_array.append(sample)
+                label_array.append(label)
+    
+    elif folder == 'valid':
+        pictures_paths = sorted(
+            [
+                os.path.join(data_dir, sname)
+                for sname in os.listdir(data_dir)
+            ]
+        )
+
+        for picture_path in pictures_paths:
+            img_sample = cv2.imread(picture_path)  # Read picture
             if img_sample is None:
                 continue
-            img_sample = img_sample[:,:,::-1] # transform from BGR 2 RGB
-            # transform the image
+            # Extract labels from a CSV file (assuming a specific format)
+            name = os.path.splitext(os.path.basename(picture_path))[0]
+            label = csv_data.loc[csv_data['ImageId'] == name, 'PredictionString']
+            label = match_labels.get(label, None)  # Use match_labels if needed
+            img_sample = img_sample[:, :, ::-1]  # Transform from BGR to RGB
             PIL_image = Image.fromarray(img_sample.astype('uint8'), 'RGB')
             sample = np.asarray(resize(PIL_image), dtype=np.uint8)
-            # append image and label to image and labels list
             img_array.append(sample)
-            label_array.append(i)
-        i+=1
+            label_array.append(label)
 
-    # print label and image array shapes
-    print(np.asarray(label_array).shape)
-    print(np.asarray(img_array).shape)
-    # return image and label arrays
+    logging.info(f"Image array shape: {np.asarray(img_array).shape}")
+    logging.info(f"Label array shape: {np.asarray(label_array).shape}")
     return np.asarray(img_array), np.asarray(label_array)
 
+
 if __name__ == '__main__':
-    for scale in range(7) :
+    # Set up logging
+    logging.getLogger().setLevel(logging.INFO)
+
+    for scale in range(11):
         folder = f"scaling_fac_{scale}"
+        logging.info(f"Processing data for {folder}")
         begin_time = datetime.datetime.now()
-        img_array, label_array = make_array(folder)
-        store_many_hdf5(img_array,label_array, folder)
-        print(datetime.datetime.now()-begin_time)
+        img_array, label_array = make_array(folder, "train")
+        store_many_hdf5(img_array, label_array, folder)
+        logging.info(f"Processing time for {folder}: {datetime.datetime.now() - begin_time}")
+
+    begin_time = datetime.datetime.now()
+    logging.info(f"Processing data for validation set")
+    img_array, label_array = make_array(os.path.join('ILSVRC', 'Data', 'CLS-LOC', 'val'), "valid")
+    store_many_hdf5(img_array, label_array, folder)
+    logging.info(f"Processing time for validation set: {datetime.datetime.now() - begin_time}")
